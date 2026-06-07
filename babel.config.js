d8d00d91da8ad4e821c92fa0c5857b0a2bae6e60
koseitutu <koaosei-tutu@ug.edu.gh>
2026-06-07 18:16:46 +0000

    build: restore clean babel config and enforce dependency overrides via lockfiles

--- babel.config.js
+++ babel.config.js
@@ -22,16 +22,6 @@ module.exports = function (api) {
     plugins,
     overrides: [
       {
-        // Target ALL files including node_modules to guarantee private fields are stripped
-        test: /[\s\S]*/,
-        plugins: [
-          ['@babel/plugin-transform-class-properties', { loose: true }],
-          ['@babel/plugin-transform-private-methods', { loose: true }],
-          ['@babel/plugin-transform-private-property-in-object', { loose: true }]
-        ]
-      },
-      {
-        // Include @fastshot/* packages for env var inlining
         include: /node_modules\/@fastshot\/(ai|auth)/,
         plugins: [
           [
